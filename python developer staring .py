bc2ed1c81c7faf1214bf5cf73a6fa78e2563e28e
Abdullah Al Emon <emon24103076@gmail.com>
2026-07-15 23:54:41 +0600

    Add files via upload
    Signed-off-by: Abdullah Al Emon <emon24103076@gmail.com>

---
+++ python developer staring .py
@@ -0,0 +1,21 @@
+'''for i in range(0, 10):
+   
+
+    if(i==3):
+        break
+    print(i)    
+
+i = 1
+while i<=10:
+    i=i+2
+
+    if(i==2):
+        continue
+    print(i) '''
+
+fruit = ["Apple", "banana", "mango", "orange"]
+
+for i in fruit:
+    print(i)
+print(help(fruit))
+print(len(fruit))
